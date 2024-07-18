81f99a68eefe7cb1f64ade2842f93eea5a208e62
Jose Carranza <jcarranza@gmail.com>
2024-07-18 11:59:07 -0600

    Publicación principal

--- src/config.js
+++ src/config.js
@@ -1,5 +1,18 @@
 export default {
+  SECRET: "recirculaappApi",
+  JWT_KEY: "the_recirculapp",
 
+  MONGO_ATLAS_PASS: 'yheGJPgElOrYcj6z', // Production
+  MONGO_ATLAS_USER: 'recirculapp', // Production
+
+  // MONGO_ATLAS_USER: "arce", // Development
+  // MONGO_ATLAS_PASS: "ukahGyRq7XkBs8JX", // Development
+
+  // MONGO_ATLAS_URI: "@cluster0.oejaozn.mongodb.net/recirculapp?retryWrites=true&w=majority", // Development
+
+  MONGO_ATLAS_URI: "@cluster0.e0vrb.mongodb.net/recirculapp?retryWrites=true&w=majority", // Production
+
+  
   smtp: {
     host: 'email-smtp.us-east-1.amazonaws.com',
     port: 587, // You can also use 25 or 2587 for STARTTLS
